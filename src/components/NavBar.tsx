62d60e31bd0e1d921872e2158f606075b5fe9179
Andrew Branch <awbranch@gmail.com>
2024-06-27 10:29:04 -0400

    Added transitions back in

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -64,7 +64,7 @@ export default function NavBar({ menus, pagePalette }: Props) {
                       <PopoverBackdrop className="fixed inset-0" />
                       <NavButton palette={palette} name={m.name} />
 
-                      {/* <Transition
+                      <Transition
                         as={Fragment}
                         enter="transition ease-out duration-200"
                         enterFrom="opacity-0 translate-y-1"
@@ -72,28 +72,28 @@ export default function NavBar({ menus, pagePalette }: Props) {
                         leave="transition ease-in duration-150"
                         leaveFrom="opacity-100 translate-y-0"
                         leaveTo="opacity-0 translate-y-1"
-                      > */}
-                      <PopoverPanel className="absolute left-0 top-full z-10 mt-3 rounded-[30px] bg-grape-700 p-6 shadow-lg ring-1 ring-gray-900/5">
-                        {m.items.map((itm) =>
-                          itm.header ? (
-                            <div
-                              className="select-none px-4 pb-1.5 pt-8 text-xs font-bold text-grape-300"
-                              key={itm._key}
-                            >
-                              {itm.name}
-                            </div>
-                          ) : (
-                            <a
-                              key={itm._key}
-                              href={itm.href}
-                              className="block whitespace-nowrap rounded-full px-4 py-1.5 text-sm font-medium text-white hover:bg-grape-500"
-                            >
-                              {itm.name}
-                            </a>
-                          ),
-                        )}
-                      </PopoverPanel>
-                      {/* </Transition> */}
+                      >
+                        <PopoverPanel className="absolute left-0 top-full z-10 mt-3 rounded-[30px] bg-grape-700 p-6 shadow-lg ring-1 ring-gray-900/5">
+                          {m.items.map((itm) =>
+                            itm.header ? (
+                              <div
+                                className="select-none px-4 pb-1.5 pt-8 text-xs font-bold text-grape-300"
+                                key={itm._key}
+                              >
+                                {itm.name}
+                              </div>
+                            ) : (
+                              <a
+                                key={itm._key}
+                                href={itm.href}
+                                className="block whitespace-nowrap rounded-full px-4 py-1.5 text-sm font-medium text-white hover:bg-grape-500"
+                              >
+                                {itm.name}
+                              </a>
+                            ),
+                          )}
+                        </PopoverPanel>
+                      </Transition>
                     </Popover>
                   )}
                 </span>
