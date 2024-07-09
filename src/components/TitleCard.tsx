d0d2554d13f6b61ff8e16708ddff47dcdc008037
Andrew Branch <awbranch@gmail.com>
2024-07-08 20:09:01 -0400

    Fixed typo in import

--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import { TitleCard as Props } from "@/types/TitleCa  rd";
+import { TitleCard as Props } from "@/types/TitleCard";
 
-export default function TitleCard({
-  eyebrow,
-  title, 
-  text
-}: Props) {
-  return <div className="rounded-lg">
-    Title Card Goes Here
-    </div>;
+export default function TitleCard({ eyebrow, title, text }: Props) {
+  return <div className="rounded-lg">Title Card Goes Here</div>;
 }
