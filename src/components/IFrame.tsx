f1bd2765ffe6f07d3a359f140c2802ba20444390
Andrew Branch <awbranch@gmail.com>
2024-07-15 21:16:12 -0400

    Added the iFrame

--- src/components/IFrame.tsx
+++ src/components/IFrame.tsx
@@ -3,10 +3,11 @@ import { IFrame as IFrameType } from "@/types/IFrame";
 
 type IFrameProps = Omit<IFrameType, "_type" | "_key">;
 
-export default function IFrame({
-  blockId,
-  blockPalette,
-  code,
-}: IFrameProps) {
-  return <div>IFRAME GOES HERE</div>;
+export default function IFrame({ code }: IFrameProps) {
+  return (
+    <div
+      className={"mx-auto flex justify-center"}
+      dangerouslySetInnerHTML={{ __html: code }}
+    />
+  );
 }
